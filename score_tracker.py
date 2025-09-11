4950da66c488980650ecc48e5eaddb9958d529bc
samuelfraley-bse <samuel.fraley@bse.eu>
2025-09-11 08:38:28 -0400

    Update score_tracker.py

--- score_tracker.py
+++ score_tracker.py
@@ -14,7 +14,7 @@ if not espn_s2 or not swid:
         espn_s2 = config["espn_s2"]
         swid = config["swid"]
 
-league = League(league_id=310285528, year=2024, espn_s2=espn_s2, swid=swid)
+league = League(league_id=31028552, year=2024, espn_s2=espn_s2, swid=swid)
 
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
